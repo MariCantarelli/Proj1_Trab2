8b5c0048b6276f94884bd4376674585459b3528d
Marina Cantarelli <111306797+MariCantarelli@users.noreply.github.com>
2025-05-18 18:36:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+//Beatriz Di Palma Carvalho, RA: 10439477
+//Luiza Marinho de Mesquita, RA: 10438045
+//Marina Cantarelli Barroca, RA: 10740412
+
+
 #include <stdio.h>      
 #include <stdlib.h>     
 #include <time.h>      
